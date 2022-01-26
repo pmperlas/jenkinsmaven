fa3c9673cf49529e3f358afc2526146e9dbcfcfc
Dani <adastra@thehackerway.com>
2022-01-26 17:12:45 +0100

    DSL Jenkins

---
+++ DSL/complete.groovy
@@ -0,0 +1,52 @@
+job('javaappDSL') {
+    description('Java Maven App con DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/Adastra-THW/jenkinsmaven.git', 'master') { node ->
+            node / gitConfigName('adastra')
+            node / gitConfigEmail('adastra@thehackerway.com')
+        }
+    }
+    parameters {
+      stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+      choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+      booleanParam('agente', false)
+    }
+    triggers {
+      cron('H/7 * * * *')
+    }
+    steps {
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('-B -DskipTests clean package')
+        }
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('test')
+        }
+        shell('''
+          echo "Entrega: Desplegando la aplicación" 
+          java -jar "/var/jenkins_home/workspace/javaappDSL/target/my-app-1.0-SNAPSHOT.jar"
+        ''')
+    }
+    publishers {
+        archiveArtifacts('target/*.jar')
+        archiveJunit('target/surefire-reports/*.xml')
+	      slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+       }
+    }
+}
